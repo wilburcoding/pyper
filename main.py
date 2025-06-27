eeeea805d24544e093ed42b892513c50d41e7b25
Wilbur W <wilburwei.2008@gmail.com>
2025-06-26 23:03:01 -0400

    Add display name entry

--- main.py
+++ main.py
@@ -8,11 +8,18 @@ root.geometry("500x500")
 tbar = tk.Frame(root, background="white", height=40,width=500)
 tbar.pack_propagate(False) 
 tbar.pack()
-
-# Create a label widget in the top bar
+name = tk.StringVar()
 
 label = tk.Label(tbar, text="Pyper v0.3", background="white", font=("Arial", 20))
 label.pack(side="left")
-
+name_bar = tk.Frame(root, background="lightgray", height=40, width=500)
+name_bar.pack_propagate(False)
+name_bar.pack()
+dnamelabel = tk.Label(name_bar, text="Display Name: ",
+                 background="lightgray", font=("Arial", 12))
+dnamelabel.pack(side="left")
+name_entry = tk.Entry(name_bar, textvariable=name,
+                      font=('Arial', 12))
+name_entry.place(relx=0.7, rely=0.5, anchor="center")
 # Run the application
 root.mainloop()
