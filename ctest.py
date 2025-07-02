89bee8d9f369a57d31ae6a37539f7318ac2c513c
Wilbur W <wilburwei.2008@gmail.com>
2025-07-02 13:09:34 -0400

    Finish most of the server side functionality

--- ctest.py
+++ ctest.py
@@ -4,15 +4,19 @@ import socket
 import json 
 HOST = "127.0.0.1" 
 PORT = 56726 
+import random
 
 s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
 #sent test message every second just for some testing (for now)
 import time
+
+s.sendall(json.dumps({"type": "join"}).encode())
 while True:
-  time.sleep(1)
-  s.sendall(json.dumps({"jj": "test"}).encode())
   data = s.recv(1024)
   print(f"Received {data!r}")
+  dat = json.loads(data.decode())
+  if (dat["state"] == "play" and dat["countdown"] == 55):
+    s.sendall(json.dumps({"type": "res", "name":str(random.randint(1,10)), "speed":str(random.randint(1,10))}).encode())
   
--- server.py
+++ server.py
@@ -3,23 +3,39 @@ import threading
 import json
 import threading
 import time
+import random
+wlist = []
+with open('words.txt', 'r') as file:
+    for line in file:
+        processed_line = line.strip()
+        if (len(processed_line) > 2 and "'" not in processed_line):
+            wlist.append(processed_line)
 HOST = "127.0.0.1"
 PORT = 56726 
-game = {}
+game = {"state":"wait", "results":{}, "words":"","countdown":0}
 conns = []
+countdown = 15
 def handle_client(conn, addr):
     print(f"Connected by {addr}")
     with conn:
-        conns.append(conn)
         while True:
             data = conn.recv(1024)  
             if not data:
                 break
             print(f"Received from {addr}: {data.decode()}")
             jdat = json.loads(data.decode())
-            payload = []
-            conn.sendall(json.dumps({"res": data.decode()}).encode()) 
-            print(f"Sent to {addr}: {data.decode()}")
+            payload = {}
+
+            if (jdat["type"] == "join" and (game["state"] == "wait" or game["state"] == "countdown")):
+              conns.append(conn)
+
+              pass
+            elif (jdat["type"] == "res"):
+                # game result
+                game["results"][jdat["name"]] = jdat["speed"]
+                pass
+            # conn.sendall(json.dumps({"res": data.decode()}).encode()) 
+            # print(f"Sent to {addr}: {data.decode()}")
 def start_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
@@ -31,10 +47,43 @@ def start_server():
 
 def manage():
     # will do game managing stuff while this is running
+    global countdown, game, conns
     while True:
-        print("This is printed every second")
+        if (game["state"] == "wait"):
+            # waiting for players
+            if (len(conns) > 0):
+                print("Starting game!")
+                game["state"] = "countdown"
+            countdown = 15
+            ranwords = [random.choice(wlist)
+                        for i in range(10)]
+            game["words"] = " ".join(ranwords)
+            game["results"] = {}
+            pass
+        elif (game["state"] == "countdown"):
+            if (countdown == 0):
+                game["state"] = "play"
+                countdown = 60
+                print("Starting game with " + str(len(conns))  + " players")
+            else:
+              countdown-=1
+        elif (game["state"] == "play"):
+            if (countdown == 0 or len(list(game["results"].keys())) == len(conns)):
+                print("Game over with results: " + str(game["results"]))
+                game["state"] = "results"
+                countdown = 15
+            else:
+              countdown-=1
+        elif (game["state"] == "results"):
+            if (countdown == 0):
+                game["state"] = "wait"
+                conns = []
+            else:
+                countdown-=1
+        game["countdown"] = countdown
+        for conn in conns:
+            conn.sendall(json.dumps(game).encode())
         time.sleep(1)
-
 
 threading.Thread(target=manage, daemon=True).start()
 
